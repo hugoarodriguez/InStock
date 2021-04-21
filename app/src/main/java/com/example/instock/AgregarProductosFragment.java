405bb4040c78093f8405063446ed26f1549be358
HugoRodriguez <hugsrodriguez8@gmail.com>
2021-04-21 04:28:46 -0600

    Se hicieron cambios de estilo y funciones en AgregarProductos

--- app/src/main/java/com/example/instock/AgregarProductosFragment.java
+++ app/src/main/java/com/example/instock/AgregarProductosFragment.java
@@ -1,9 +1,12 @@
 package com.example.instock;
 
 import android.content.Intent;
+import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.instock.Adapter.CategoriasAdaptador;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,9 +35,11 @@ public class AgregarProductosFragment extends Fragment {
 
     public AgregarProductosFragment(){}
     ImageView imgProducto;
-    Button btnImage;
+    Button btnImage, btnAgregar;
+    TextInputLayout tilNombrePro, tilCantPro, tilPrecioPro, tilDetallesPro;
+    EditText edtNombrePro, edtCantPro, edtPrecioPro, edtDetallesPro;
 
-
+    String mensajeAlerta = "Daro requerido";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,11 +52,13 @@ public class AgregarProductosFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_agregar_productos, container, false);
-        imgProducto = (ImageView) vista.findViewById(R.id.imgProducto);
-        btnImage = (Button)vista.findViewById(R.id.btnImage);
+        enlazarVistas(vista);//Enlazamos las vistas
 
         //Método para seleccionar imagen
         SelectIMG();
+
+        //Método a ejecutar para el btnAgregar
+        agregar();
 
         return vista;
     }
@@ -58,8 +66,27 @@ public class AgregarProductosFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
 
+    //Enlazar vistas
+    public void enlazarVistas(View v){
+        imgProducto = (ImageView) v.findViewById(R.id.imgProducto);
+        btnImage = (Button)v.findViewById(R.id.btnImage);
+        btnAgregar = (Button)v.findViewById(R.id.btnAgregar);
+        tilNombrePro = (TextInputLayout)v.findViewById(R.id.tilNombrePro);
+        tilCantPro = (TextInputLayout)v.findViewById(R.id.tilCantPro);
+        tilDetallesPro = (TextInputLayout)v.findViewById(R.id.tilDetallesPro);
+        tilPrecioPro = (TextInputLayout)v.findViewById(R.id.tilPrecioPro);
+        edtCantPro = (EditText) v.findViewById(R.id.edtCantPro);
+        edtDetallesPro = (EditText)v.findViewById(R.id.edtDetallesPro);
+        edtNombrePro = (EditText)v.findViewById(R.id.edtNombrePro);
+        edtPrecioPro = (EditText)v.findViewById(R.id.edtPrecioPro);
 
+        //Método para quitar alerta en onChanged
+        edtChangeListener(edtNombrePro, tilNombrePro);
+        edtChangeListener(edtCantPro, tilCantPro);
+        edtChangeListener(edtPrecioPro, tilPrecioPro);
+        edtChangeListener(edtDetallesPro, tilDetallesPro);
     }
 
     //Objeto que permite seleccionar archivos y por medio del cual se asigna la imagen al objeto imProducto
@@ -87,6 +114,63 @@ public class AgregarProductosFragment extends Fragment {
             public void onClick(View v) {
                 //Abrimos la galería de imágenes
                 gcSeleccionarImagen.launch("image/*");
+            }
+        });
+    }
+
+    //Método para enlazar los editText con el ChangedListener
+    private void edtChangeListener(EditText editText, TextInputLayout til){
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() > 0){
+                    til.setError(null);
+                }
+                else{
+                    til.setError(mensajeAlerta);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void agregar(){
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(edtNombrePro.getText().toString().equals("")
+                        || edtCantPro.getText().toString().equals("")
+                        || edtPrecioPro.getText().toString().equals("")
+                        || edtDetallesPro.getText().toString().equals("")){
+
+                    if(edtNombrePro.getText().toString().equals("")){
+                        tilNombrePro.setError(mensajeAlerta);
+                    }
+                    if(edtCantPro.getText().toString().equals("")){
+                        tilCantPro.setError(mensajeAlerta);
+                    }
+                    if(edtPrecioPro.getText().toString().equals("")){
+                        tilPrecioPro.setError(mensajeAlerta);
+                    }
+                    if(edtDetallesPro.getText().toString().equals("")){
+                        tilDetallesPro.setError(mensajeAlerta);
+                    }
+                } else {
+                    Toast.makeText(getContext(), "¡Agregar!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
