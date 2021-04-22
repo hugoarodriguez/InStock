b4727e3f32071ae52a924e815f70a47051c00830
Sofia Alvarado <alvaradosofiah@outlook.com>
2021-04-21 21:43:19 -0600

    Guardando cambios

--- app/src/main/java/com/example/instock/VerReservasFragment.java
+++ app/src/main/java/com/example/instock/VerReservasFragment.java
@@ -3,62 +3,39 @@ package com.example.instock;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VerReservasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.instock.Adapter.Producto;
+import com.example.instock.Adapter.ProductoAdaptadpr;
+
+import java.util.ArrayList;
+
+
 public class VerReservasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerReserva;
+    ProductoAdaptadpr ReservaAdaptador;
+    ArrayList<Producto> ReservaList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public VerReservasFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VerReservasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VerReservasFragment newInstance(String param1, String param2) {
-        VerReservasFragment fragment = new VerReservasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ver_reservas, container, false);
+        View vista = inflater.inflate(R.layout.fragment_ver_reservas, container, false);
+
+        return vista;
     }
 }
